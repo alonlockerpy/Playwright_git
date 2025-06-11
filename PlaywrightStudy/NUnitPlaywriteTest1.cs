55250b362ee70ab28630da401f335839db7a2d04
AlonL <appleuser@AppleUsers-MacBook-Pro.local>
2025-06-11 11:14:16 +0300

    Test with NUnit page class use

---
+++ PlaywrightStudy/NUnitPlaywriteTest1.cs
@@ -0,0 +1,31 @@
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+
+namespace PlaywrightDemo;
+
+public class NUnitPlaywriteTests : PageTest
+{
+    [SetUp]
+    public async Task Setup()
+    {
+        await Page.GotoAsync(url: "http://www.eaapp.somee.com");
+    }
+
+    [Test]
+    public async Task Test1()
+    {
+       
+        await Page.ClickAsync(selector: "text=Login");
+        
+        await Page.FillAsync("#UserName", "admin");
+        await Page.FillAsync("#Password", "password");
+        await Page.ClickAsync("text=Log in");
+        await Page.ScreenshotAsync(new PageScreenshotOptions
+        {
+            Path = "EmployeeDetails.jpg"
+        });
+        await Expect(Page.Locator("text='Employee Details'")).ToBeVisibleAsync();
+    }
+}
+//should set only this test as active
+//run form Terminal: HEADED=1 dotnet test
